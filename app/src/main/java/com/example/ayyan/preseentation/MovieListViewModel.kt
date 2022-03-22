011066a46b87df91eb42ac4f9f498215c44ca64f
aravindus <aravindus6@gmail.com>
2022-03-22 11:11:52 +0530

    Retrofit with MVVM Architecture

--- app/src/main/java/com/example/ayyan/preseentation/MovieListViewModel.kt
+++ app/src/main/java/com/example/ayyan/preseentation/MovieListViewModel.kt
@@ -1,12 +1,8 @@
 package com.example.ayyan.preseentation
 
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.ayyan.common.Resources
 import com.example.ayyan.domain.model.MovieData
 import com.example.ayyan.domain.use_case.MovieListUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -17,12 +13,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MovieListViewModel @Inject constructor(
-    private val getMovieListUseCase: MovieListUseCase,
-    private val savedStateHandle: SavedStateHandle
+    private val getMovieListUseCase: MovieListUseCase
 ) : ViewModel() {
-
-/*    private val movieListState = mutableStateOf(MovieListState())
-    val state: State<MovieListState> = movieListState*/
     val movieList = MutableLiveData<List<MovieData>>()
 
     init {
@@ -31,29 +23,7 @@ class MovieListViewModel @Inject constructor(
 
     private fun getMovieListViewModel() {
         getMovieListUseCase().onEach { resources ->
-            when (resources) {
-                is Resources.Success -> {
-                    movieList.postValue(resources.data ?: emptyList())
-                }
-            }
+            movieList.postValue(resources.data ?: emptyList())
         }.launchIn(viewModelScope)
-
-
-        /* private fun getMovieListViewModel() {
-         getMovieListUseCase().onEach { resources ->
-             when (resources) {
-                 is Resources.Success -> {
-                     movieListState.value = MovieListState(movieList = resources.data ?: emptyList())
-                 }
-                 is Resources.Error -> {
-                     movieListState.value =
-                         MovieListState(error = resources.message ?: "An error occured")
-                 }
-                 is Resources.Loading -> {
-                     movieListState.value = MovieListState(isLoading = true)
-                 }
-             }
-         }.launchIn(viewModelScope)
-     }*/
     }
 }
